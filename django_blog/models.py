ce7f7ee963d0fdac4e4f68476671a54ef193be1d
Sh_PSR <shahin1974_27@yahoo.com>
2021-01-28 01:24:02 +0330

    modify models and debug

--- django_blog/models.py
+++ django_blog/models.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField('نام', max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField('عنوان', max_length=20)
     text = models.TextField('متن')
@@ -11,12 +18,13 @@ class Post(models.Model):
     date_created = models.DateTimeField('تاریخ ثبت')
     date_published = models.DateTimeField('تاریخ انتشار')
     tags = models.ManyToManyField('Tag')
-    author = models.ForeignKey('نویسنده', User, on_delete=models.CASCADE)
-    category = models.ForeignKey('دسته بندی', 'Category', on_delete=models.CASCADE)
+    author = models.ForeignKey(verbose_name='نویسنده', to=User, on_delete=models.CASCADE)
+    category = models.ForeignKey(verbose_name='دسته بندی', to=Category, on_delete=models.CASCADE)
     likes = models.ManyToManyField(
         User,
         through='LikePost',
-        through_fields=('post', 'user')
+        through_fields=('post', 'author'),
+        related_name='post_likes'
     )
 
     def __str__(self):
@@ -24,7 +32,7 @@ class Post(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField('نام')
+    name = models.CharField('نام', max_length=20)
 
     def __str__(self):
         return self.name
@@ -35,12 +43,13 @@ class Comment(models.Model):
     is_published = models.BooleanField('وضعیت انتشار')
     date_create = models.DateTimeField('تاریخ ثبت')
     date_published = models.DateTimeField('تاریخ انتشار')
-    post = models.ForeignKey('مطلب', Post, on_delete=models.CASCADE)
-    author = models.ForeignKey('نویسنده', User, on_delete=models.CASCADE)
+    post = models.ForeignKey(verbose_name='مطلب', to=Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(verbose_name='نویسنده', to=User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(
         User,
         through='LikeComment',
-        through_fields=('post', 'author')
+        through_fields=('comment', 'author'),
+        related_name='comment_likes'
     )
 
     def __str__(self):
@@ -49,24 +58,17 @@ class Comment(models.Model):
 
 class LikePost(models.Model):
     is_liked = models.BooleanField('وضعیت پسندیدن')
-    post = models.ForeignKey('مطلب', Post, on_delete=models.CASCADE)
-    author = models.ForeignKey('نظر دهنده', User, on_delete=models.CASCADE)
+    post = models.ForeignKey(verbose_name='مطلب', to=Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(verbose_name='نظر دهنده', to=User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.is_liked
 
 
-class Category(models.Model):
-    name = models.CharField('نام', max_length=20)
-
-    def __str__(self):
-        return self.name
-
-
 class LikeComment(models.Model):
     is_liked = models.BooleanField('وضعیت پسندیدن')
-    comment = models.ForeignKey('نظر', Comment, on_delete=models.CASCADE)
-    author = models.ForeignKey('نویسنده', User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(verbose_name='نظر', to=Comment, on_delete=models.CASCADE)
+    author = models.ForeignKey(verbose_name='نویسنده', to=User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.is_liked
