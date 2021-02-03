27682aeece61438ba8500ab92387746c3785b2ba
Sh_PSR <shahin1974_27@yahoo.com>
2021-02-04 02:44:11 +0330

    add PostModelAdmin

--- django_blog/admin.py
+++ django_blog/admin.py
@@ -3,7 +3,18 @@ from django_blog.models import (
     Post, Tag, LikeComment, Comment, Category, LikePost
 )
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    exclude = ('date_created', 'date_published')
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        if not admin.ModelAdmin.has_change_permission(self, request):
+            self.__class__.exclude = ('is_active', 'is_published', 'date_created', 'date_published')
+        return super().get_form(request, obj, change, **kwargs)
+
+
+# admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(LikeComment)
 admin.site.register(Comment)
