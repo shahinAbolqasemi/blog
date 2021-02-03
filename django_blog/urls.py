885c06c754ab527fcdb05274ad25f328a3f7bc35
Sh_PSR <shahin1974_27@yahoo.com>
2021-02-04 02:43:03 +0330

    add PostViewUrl

---
+++ django_blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from django_blog import views
+
+app_name = 'django_blog'
+urlpatterns = [
+    path('post/<int:pk>/', views.PostView.as_view(), name='post'),
+]
