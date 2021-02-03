82140a0811397aaa3fb121628ba8a139dcc99254
Sh_PSR <shahin1974_27@yahoo.com>
2021-02-04 02:50:30 +0330

    add PostView to view

--- django_blog/views.py
+++ django_blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import (
+    Post
+)
 
-# Create your views here.
+
+class PostView(generic.DetailView):
+    model = Post
+    template_name = 'django_blog/post.html'
