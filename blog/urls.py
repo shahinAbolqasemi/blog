1a1cdc2b96ac4a8dae5a8d778b01092233f13b18
Sh_PSR <shahin1974_27@yahoo.com>
2021-02-10 20:50:36 +0330

    add index page

--- blog/urls.py
+++ blog/urls.py
@@ -18,7 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from django_blog.views import index_view
+
 urlpatterns = [
+    path('', index_view, name='index'),
     path('admin/', admin.site.urls),
     path('blog/', include('django_blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
