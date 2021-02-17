bde56d7cef1a417cb9ae45824c04130505b595c4
Sh_PSR <shahin1974_27@yahoo.com>
2021-02-17 23:54:43 +0330

    add tag url

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -6,4 +6,5 @@ from django_blog import views
 app_name = 'django_blog'
 urlpatterns = [
     path('post/<int:pk>/', views.PostView.as_view(), name='post'),
+    path('tag/<int:pk>/', views.TagPostsList.as_view(), name='tag'),
 ]
